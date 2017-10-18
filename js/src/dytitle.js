394f1d8677d33e9def9f07a66fa3d8e766de3eeb
KLEIST <lingyu2586@gmail.com>
2017-10-18 13:11:10 +0800

    Site updated: 2017-10-18 13:11:10

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -3,12 +3,12 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
     if (document.hidden) {
         $('[rel="icon"]').attr('href', "/img/TEP.ico");
-        document.title = '(つд∩) 我藏起来啦 ';
+        document.title = '(つд∩) 我藏起来啦 | ';
         clearTimeout(titleTime);
     }
     else {
         $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = ' (≥∇≤) 你找到我啦 ' + OriginTitile;
+        document.title = ' (●′ω`●) 你找到我啦 | ' + OriginTitile;
         titleTime = setTimeout(function () {
             document.title = OriginTitile;
         }, 2000);
