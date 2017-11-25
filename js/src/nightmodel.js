c528a6cc7f3c81a21f4c9553bb8def2a12b81da7
KLEIST <lingyu2586@gmail.com>
2017-11-25 15:28:09 +0800

    Site updated: 2017-11-25 15:28:08

--- js/src/nightmodel.js
+++ js/src/nightmodel.js
@@ -1,71 +1,71 @@
 ;
-$("#nightmodel").click(function(){
-    document.getElementById('switch_mode').classList.toggle("nightmodel");
-    if(document.getElementById('switch_mode').classList.contains("nightmodel")) {
-        localStorage.setItem("night",true);
-    }
-    else {
-        localStorage.setItem("night",false);
-    }
-    dayModeState();
+$("#nightmodel").click(function() {
+  document.getElementById('switch_mode').classList.toggle("nightmodel");
+  if (document.getElementById('switch_mode').classList.contains("nightmodel")) {
+    localStorage.setItem("night", true);
+  } else {
+    localStorage.setItem("night", false);
+  }
+  dayModeState();
 });
 
 function dayModeState() {
-    var night=localStorage.getItem('night');
-    console.log("night_mode:" + night);
-    if(night==="true") {
-        // document.getElementById('switch_mode').classList.add("nightmodel");
-        switchmode_moon ();
-        document.getElementById('nightmodel').innerHTML='<i id = "moon" class="fa fa-lightbulb-o fa-fw"></i>'
-    }
-
-   else {
-        // document.getElementById('switch_mode').classList.remove("nightmodel");
-        switchmode_sun ();
-        document.getElementById('nightmodel').innerHTML='<i id = "moon" class="fa fa-moon-o fa-fw"></i>'
-    }
+  var night = localStorage.getItem('night');
+  console.log("night_mode:" + night);
+  if (night === "true") {
+    // document.getElementById('switch_mode').classList.add("nightmodel");
+    switchmode_moon();
+    document.getElementById('nightmodel').innerHTML = '<i id = "moon" class="fa fa-lightbulb-o fa-fw"></i>'
+  } else {
+    // document.getElementById('switch_mode').classList.remove("nightmodel");
+    switchmode_sun();
+    document.getElementById('nightmodel').innerHTML = '<i id = "moon" class="fa fa-moon-o fa-fw"></i>'
+  }
 }
 
 window.onload = dayModeState();
 
 // --------------------------------------------
 
-function switchmode_sun () {
-    $("body").removeClass("night_big_body");
-    $(".content-wrap").removeClass("night_big_block");
-    $(".header-inner").removeClass("night_big_block");
-    $(".sidebar-inner").removeClass("night_big_block");
-    $(".headband").removeClass("night_small_meta");
-    $(".site-meta").removeClass("night_small_meta");
-    $(".posts-expand .post-title-link").removeClass("night_small_line");
-    $(".posts-expand .post-eof").removeClass("night_small_line");
-    $(".menu-item-active a").removeClass("night_small_menu_active");
-    $(".menu .menu-item a").removeClass("night_small_menu");
-    $(".site-author-name").removeClass("night_small_authorname");
-    $(".post-button .btn").removeClass("night_small_btn");
-    $(".pagination .page-number.current").removeClass("night_small_number_current");
-    $(".pagination .page-number").removeClass("night_small_number");
-    $(".next").removeClass("night_small_number");
-    $("a.author").removeClass("night_small_beian");
-    $(".links-of-author-item a").removeClass("night_small_author_item");
+function switchmode_sun() {
+  $("body").removeClass("night_big_body");
+  $(".post-body").removeClass("night_model_post_body");
+  $(".content-wrap").removeClass("night_big_block");
+  $(".header-inner").removeClass("night_big_block");
+  $(".sidebar-inner").removeClass("night_big_block");
+  $(".headband").removeClass("night_small_meta");
+  $(".site-meta").removeClass("night_small_meta");
+  $(".posts-expand .post-title-link").removeClass("night_small_line");
+  $(".posts-expand .post-eof").removeClass("night_small_line");
+  $(".menu-item-active a").removeClass("night_small_menu_active");
+  $(".menu .menu-item a").removeClass("night_small_menu");
+  $(".site-author-name").removeClass("night_small_authorname");
+  $(".post-button .btn").removeClass("night_small_btn");
+  $(".pagination .page-number.current").removeClass("night_small_number_current");
+  $(".pagination .page-number").removeClass("night_small_number");
+  $(".next").removeClass("night_small_number");
+  $("a.author").removeClass("night_small_beian");
+  $(".links-of-author-item a").removeClass("night_small_author_item");
 }
+
 function switchmode_moon() {
-    $("body").addClass("night_big_body");
-    $(".headband").addClass("night_small_meta");
-    $(".content-wrap").addClass("night_big_block");
-    $(".header-inner").addClass("night_big_block");
-    $(".sidebar-inner").addClass("night_big_block");
-    $(".menu .menu-item a").addClass("night_small_menu");
-    $(".menu-item-active a").addClass("night_small_menu_active");
-    $(".menu-item-active a").removeClass("night_small_menu");
-    $(".posts-expand .post-title-link").addClass("night_small_line");
-    $(".posts-expand .post-eof").addClass("night_small_line");
-    $(".site-meta").addClass("night_small_meta");
-    $(".post-button .btn").addClass("night_small_btn");
-    $(".pagination .page-number.current").addClass("night_small_number_current");
-    $(".pagination .page-number").addClass("night_small_number");
-    $(".next").addClass("night_small_number");
-    $("a.author").addClass("night_small_beian");
-    $(".links-of-author-item a").addClass("night_small_author_item");
-    $(".site-author-name").addClass("night_small_authorname");
+  $("body").addClass("night_big_body");
+  $(".post-body").addClass("night_model_post_body");
+  $(".headband").addClass("night_small_meta");
+  $(".content-wrap").addClass("night_big_block");
+  $(".header-inner").addClass("night_big_block");
+  $(".sidebar-inner").addClass("night_big_block");
+  $(".menu .menu-item a").addClass("night_small_menu");
+  $(".menu-item-active a").addClass("night_small_menu_active");
+  $(".menu-item-active a").removeClass("night_small_menu");
+  $(".posts-expand .post-title-link").addClass("night_small_line");
+  $(".posts-expand .post-eof").addClass("night_small_line");
+  $(".site-meta").addClass("night_small_meta");
+  $(".post-button .btn").addClass("night_small_btn");
+  $(".pagination .page-number.current").addClass("night_small_number_current");
+  $(".pagination .page-number").addClass("night_small_number");
+  $(".next").addClass("night_small_number");
+  $("a.author").addClass("night_small_beian");
+  $(".links-of-author-item a").addClass("night_small_author_item");
+  $(".site-author-name").addClass("night_small_authorname");
 }
