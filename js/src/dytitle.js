a6cf19e8593fe38058624c5fd2303878460e598f
KLEIST <lingyu2586@gmail.com>
2018-03-15 15:44:00 +0800

    Site updated: 2018-03-15 15:43:59

--- js/src/dytitle.js
+++ js/src/dytitle.js
@@ -19,7 +19,7 @@ document.addEventListener('visibilitychange', function () {
 var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善","道可道","非常道");
+var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善","欢迎月月鸟","还点？(＃°Д°)","你还点！(╬ Ò ‸ Ó)","再点我就报警啦o(￣ヘ￣o＃) ","嘤嘤嘤(*/ω＼*)");
 var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
 var x = e.pageX,
